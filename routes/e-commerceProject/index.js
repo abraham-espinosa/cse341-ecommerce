05db231c609390de565738346d80a36992d3c001
abraham-1 <esp19005@byui.edu>
2021-10-02 23:21:21 -0500

    project

--- routes/e-commerceProject/index.js
+++ routes/e-commerceProject/index.js
@@ -1,11 +1,12 @@
 const routes = require('express').Router();
 
 routes
-      .use('/e-commercer', require('./views/shop/index'))
-      .get('/', (req, res, next) => {
-          res.render('pages/ecommerceProject/', {
-              pageTitle: 'E-commerce Project',
-              path: '/ecommerceProject'});
-      });
+    .use('/e-commercer', require('./views/shop/index'))
+    .get('/', (req, res, next) => {
+        res.render('pages/ecommerceProject/', {
+            pageTitle: 'E-commerce Project',
+            path: '/ecommerceProject'
+        });
+    });
 
 module.exports = routes;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const routes = require('express').Router();
+const classActivities = require('./classRoutes');
 const teamActivities = require('./teamRoutes');
+const proveActivities = require('./proveRoutes');
 const ecommerceProject = require('./e-commerceProject');
 
 routes
+    .use('/classActivities', classActivities)
     .use('/teamActivities', teamActivities)
+    .use('/proveActivities', proveActivities)
     .use('/ecommerceProject', ecommerceProject)
 
     .get('/', (req, res, next) => {
