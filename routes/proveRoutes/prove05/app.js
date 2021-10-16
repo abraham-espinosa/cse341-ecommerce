de831874c63f0adac290a94713379ece91c7ce8b
abraham-1 <esp19005@byui.edu>
2021-10-16 17:06:41 -0500

    push

--- routes/proveRoutes/prove05/app.js
+++ routes/proveRoutes/prove05/app.js
@@ -68,7 +68,7 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    app.listen(process.env.PORT || 5000);
+    app.listen(5000);
   })
   .catch(err => {
     console.log(err);
